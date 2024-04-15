04c00fcc85852113d229839ccf2ef8ecd1c1b63f
Zora <137242094+zora-san@users.noreply.github.com>
2024-04-15 10:06:18 -0700

    Update Quiz_3.asm

--- Quiz_3.asm
+++ Quiz_3.asm
@@ -1,1 +1,25 @@
+section .text
+        global _start
 
+_start:
+        mov ecx,5               ;counter
+        mov eax,[factorial]     
+        jmp multiply
+        jmp exit
+
+multiply:
+        imul eax,ecx
+        dec ecx
+        jnz multiply
+        jmp exit
+
+exit:
+        mov [result],eax
+        mov eax,1
+        int 0x80
+
+section .data
+        factorial dd 6
+
+segment .bss
+        result resb 1
